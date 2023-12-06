726470adbe7484801ba401cdf301c7dbcd9256db
RITIK TAYAL <tayalritik1002@gmail.com>
2023-12-06 12:12:41 +0530

    Time: 0 ms (100.00%) | Memory: 6.9 MB (98.23%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
-
-
-
     string removeOccurrences(string s, string part) {
-   
-    //ITERATIVE APPROCH
     int found=s.find(part);
     while(found!=string::npos){
         s.erase(found,part.length());
         found=s.find(part);
     }
-    
-    return s;
-        
+    return s;  
     }
 };
