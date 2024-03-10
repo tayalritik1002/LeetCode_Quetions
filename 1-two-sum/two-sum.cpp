07a139522cc557f589e424eceb1fa1009a150804
RITIK TAYAL <tayalritik1002@gmail.com>
2024-03-10 20:06:27 +0530

    Time: 8 ms (79.79%) | Memory: 14.1 MB (13.81%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
-static bool cmp(pair<int,int>a,pair<int,int>b){
-    return a.first <b.first;
-}
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>ans;
-        vector<pair<int,int>>v;
+        unordered_map<int,int>mp;
         for(int i=0;i<nums.size();i++){
-            int value=nums[i];
-            int index=i;
-            v.push_back({value,index});
+            int currElement=nums[i];
+            int remainingNum=target-currElement;
+            if(mp.find(remainingNum)!=mp.end()) return {mp[remainingNum],i};
+            mp[currElement]=i;
         }
-        sort(v.begin(),v.end(),cmp);
-        int s=0;
-        int e=v.size()-1;
-        while(s<e){
-            if(v[s].first+v[e].first==target){
-                ans.push_back(v[s].second);
-                ans.push_back(v[e].second);
-                break;
-            }
-            else if(v[s].first+v[e].first>target)e--;
-            else s++;
-        }
-        return ans;
-
+        return {-1,-1};
+        
     }
 };
