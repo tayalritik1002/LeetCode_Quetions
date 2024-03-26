01281d28c111a6b10f9fa2209f2f3a4dc8f67ada
RITIK TAYAL <tayalritik1002@gmail.com>
2024-03-26 21:30:42 +0530

    Time: 30 ms (97.12%) | Memory: 43.6 MB (55.21%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,51 +1,21 @@
 class Solution {
 public:
-    vector<int> findSmallestPositive(vector<int>& nums) {
-        int n = nums.size(); 
-        int mini = INT_MAX; 
-        int maxi = INT_MIN;
-        vector<int> ans;
-        
-        for (int i = 0; i < n; i++) {
-            if (nums[i] >= 0) {
-                mini = min(nums[i], mini);
-                maxi = max(maxi, nums[i]);
-            }
-        }
-        
-        ans.push_back(mini);
-        ans.push_back(maxi);
-        return ans;
-    }
+  
 
     int firstMissingPositive(vector<int>& nums) {
         int n=nums.size();
-        vector<int> p = findSmallestPositive(nums);
-        
-        if (p[0] == INT_MAX && p[1] == INT_MIN) {
-            return 1; 
-        }
-        if(p[0]==0 && p[1]==0){
-            return 1;
+        nums.push_back(0);
+        // replace to all positive to thier correct position coresponding index
 
-        } 
-
-        // if(p[0]==0 && p[1]>1)return 1;
-
-        // if(p[0]==0 && p[1]==1)return 2;
-
-        if(p[0]>1 && p[1]>1)return 1;
-        unordered_map<int,int>mp;
         for(int i=0;i<n;i++){
-            mp[nums[i]]++;
+            while(nums[i]>0 && nums[i]<=n && nums[i]!=nums[nums[i]])swap(nums[i],nums[nums[i]]);
+        }
+        // iterate the loop fro 1 to n if any element not present then return 
+        for(int i=1;i<=n;i++){
+           if( nums[i]!=i)return i;
         }
 
-        for(int i=p[0];i<=p[1];i++){
-            if(mp.find(i)!=mp.end())continue;
-            return i;
-        }
-
-        return p[1]+1;
+        return n+1;   
         
     }
 };
