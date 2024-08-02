36ef095d17b900415c6059be03fd60b4461a65bb
RITIK TAYAL <tayalritik1002@gmail.com>
2024-08-02 06:18:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (16.18%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-    int found=s.find(part);
+        int found=s.find(part);
     while(found!=string::npos){
         s.erase(found,part.length());
         found=s.find(part);
     }
-    return s;  
+    return s;
+        
     }
 };
