599eaa716eda4eeaf20bc8b1c394785130368211
RITIK TAYAL <tayalritik1002@gmail.com>
2024-09-17 19:21:51 +0530

    Time: 3 ms (28.15%) | Memory: 8.3 MB (45.93%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int reverse(int x) {
-        int ans=0,rem;
+        int ans=0;
         while(x){
-            rem=x%10;
+            int rem=x%10;
             x/=10;
-            if(ans>INT_MAX/10 || ans<INT_MIN/10)return 0;
+            if((ans>INT_MAX/10) || (ans<INT_MIN/10))return 0;
             ans=ans*10+rem;
+
         }
-        return ans;
+    
+        return ans;   
     }
 };
