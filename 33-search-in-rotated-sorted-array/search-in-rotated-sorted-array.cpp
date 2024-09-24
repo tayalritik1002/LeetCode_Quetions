f8fca658a0358889d2597c037bedae112daa24ea
RITIK TAYAL <tayalritik1002@gmail.com>
2024-09-24 20:32:18 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (50.38%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int  s=0;
+        int e=nums.size()-1;
+        int mid;
+
+        while(s<=e){
+            mid=s+(e-s)/2;
+            if(nums[mid]==target)return mid;
+            // left part is sorted
+            if(nums[s]<=nums[mid]){
+                if(nums[s]<=target && target<=nums[mid])e=mid-1;
+                else s=mid+1;
+
+            }
+            // right part is sorted
+            else{
+                 if(nums[mid]<=target && target<=nums[e])s=mid+1;
+                else e=mid-1;
+
+            }
+        }
+        return -1;
+        
+    }
+};
