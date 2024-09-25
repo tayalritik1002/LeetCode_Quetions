a5c769e99deff7e96eb1173951c307ab469ab993
RITIK TAYAL <tayalritik1002@gmail.com>
2024-09-25 16:20:52 +0530

    Time: 3 ms (99.09%) | Memory: 10.2 MB (74.39%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,43 +1,45 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int lenS=s.length();
-        int lenT=t.length();
-        int ansindex=-1;
-        int anslen=INT_MAX;
-        int  start=0;
-        if(lenT>lenS)return "";
-        unordered_map<char,int>mps;
-        unordered_map<char,int>mpt;
-        // to keep track string t chars
-        for(int i=0;i<lenT;i++){
-            char ch=t[i];
-            mpt[ch]++;
+          if (s.empty() || t.empty()) {
+            return "";
         }
-        int count=0;
-        for(int i=0;i<s.length();i++){
-            char ch=s[i];
-            mps[ch]++;
-            // count increase if valid charcter jo pattern me ho
-            if(mps[ch]<=mpt[ch])count++;
+        vector<int>hash(256,0);
 
-            if(count==lenT){
-                // minimize the window-> freq decrement, ans update, start ko aage badhana
-                while(mps[s[start]]>mpt[s[start]] || mpt[s[start]]==0){
-                    if(mps[s[start]]>mpt[s[start]])mps[s[start]]--;
-                    start++;
-                }
-                // answer update
-                int lenOfWindow=i-start+1;
-                if(lenOfWindow<anslen){
-                    anslen=lenOfWindow;
-                    ansindex=start;
-                }
+        int l=0,r=0,minlen=INT_MAX,sind=-1,cnt=0;
+
+        int n=s.size(),m=t.size();
+
+        for(int i=0;i<m;i++){
+            hash[t[i]]++;
+        }
+
+        while(r<n){
+
+            if(hash[s[r]]>0){
+               cnt++;
             }
+            hash[s[r]]--;
+            
+            while(cnt==m){
 
+                if(r-l+1<minlen){
+                    minlen=r-l+1;
+                    sind=l;
+                }
+                hash[s[l]]++;
+
+                if(hash[s[l]]>0){
+                    cnt=cnt-1;
+                }
+
+                l++;
+            }
+            r=r+1;
         }
-        if(ansindex==-1)return "";
-        else return s.substr(ansindex,anslen);
- 
+        
+        return sind==-1?"":s.substr(sind,minlen);
     }
+        
+    
 };
