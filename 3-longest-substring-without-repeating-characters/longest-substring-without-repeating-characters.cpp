da5433ffca703cdc59721e7df89c73f9e374a40e
RITIK TAYAL <tayalritik1002@gmail.com>
2024-09-25 09:31:22 +0530

    Time: 10 ms (72.40%) | Memory: 11.2 MB (79.95%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,16 +2,26 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int maxlen=0; int st=s.length();
+        if(s.length()==1)return 1;
+        
 
-        for(int i=0;i<st;i++){
-            int hash[256]={0};
-            for(int j=i;j<st;j++){
-                if(hash[s[j]]==1)break;
-                int len=j-i+1;
-                maxlen=max(maxlen,len);
-                hash[s[j]]=1;
+        int l=0; int r=0;
+        vector<int>hash(256,-1);
+        // cout<<hash[10];
+
+        while(r<st){
+            if(hash[s[r]]!=-1){
+                if(hash[s[r]]>=l){
+                    l=hash[s[r]]+1;
+                }
             }
+            int len=r-l+1;
+             maxlen=max(maxlen,len);
+            hash[s[r]]=r;
+            r++;
         }
+
+       
         return maxlen;
     }
 };
