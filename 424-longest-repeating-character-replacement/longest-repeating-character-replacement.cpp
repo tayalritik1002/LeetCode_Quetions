341812d19e4f3750f6b14e75b4c1f02f31d75894
RITIK TAYAL <tayalritik1002@gmail.com>
2024-09-25 13:12:42 +0530

    Time: 5 ms (80.51%) | Memory: 8.5 MB (54.60%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -3,19 +3,32 @@ public:
     int characterReplacement(string s, int k) {
 
         int max_ans=0; int n=s.length();
-        for(int i=0;i<n;i++){
-            int hash[26]={0};
-            int max_freq=0;
-            for(int j=i;j<n;j++){
-                hash[s[j]-'A']++;
-                max_freq=max(max_freq,hash[s[j]-'A']);
-                
-                int changes= (j-i+1)-max_freq;
-                
-                if(changes<=k){max_ans=max(max_ans,j-i+1);}
-                else break;
+        int l=0;int r=0;int max_freq=0; int hash[26]={0};
+       
+        while(r<n){
+             
+            hash[s[r]-'A']++;
+            max_freq=max(max_freq,hash[s[r]-'A']);
+            
+
+            while((r-l+1)-max_freq>k){
+                hash[s[l]-'A']--;
+                max_freq=0;
+                for(int p=0;p<26;p++){
+                    max_freq=max(max_freq,hash[p]);
+                }
+                l=l+1;
             }
+             
+            if((r-l+1)-max_freq<=k){
+                max_ans=max(max_ans,(r-l+1));
+               
+            }
+            r++;
+
         }
+
+       
         return max_ans;
     }
 };
