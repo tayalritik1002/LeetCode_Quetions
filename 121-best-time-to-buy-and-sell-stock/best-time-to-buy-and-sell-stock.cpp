9951d8ced2981b9c231728daac3756ae921320e0
RITIK TAYAL <tayalritik1002@gmail.com>
2024-11-14 11:50:32 +0530

    Time: 3 ms (29.98%) | Memory: 96 MB (89.73%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,22 +1,16 @@
 class Solution {
-   
 public:
-int solve(vector<int>& arr,int& mini,int& maxprofit){
-    // base case
-  for(int i=0;i<arr.size();i++){
-    mini=min(mini,arr[i]);
-    int profit=arr[i]-mini;
-    maxprofit=max(maxprofit,profit);
-
-  }
-  return maxprofit;
-
-}
     int maxProfit(vector<int>& arr) {
+        int n=arr.size();
         int mini=arr[0];
-        int maxi=0;
-        int ans=solve(arr,mini,maxi);
-        return ans;
-    }
+        int answer=INT_MIN;
+        if(n==1)return 0;
 
+        for(int i=1;i<n;i++){
+            mini=min(mini,arr[i]);
+            answer=max(answer,arr[i]-mini);
+
+        }
+        return answer;
+    }
 };
