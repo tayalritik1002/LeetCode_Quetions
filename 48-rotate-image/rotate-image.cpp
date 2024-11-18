126a86741ba2416f610c72090bb28f44c5a36111
RITIK TAYAL <tayalritik1002@gmail.com>
2024-11-18 13:14:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (12.19%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -9,8 +9,17 @@ public:
             }
         }
 
+        // for(int i=0;i<n;i++){
+        //     reverse(matrix[i].begin(),matrix[i].end());
+        // }
+
+        // reverse the array
         for(int i=0;i<n;i++){
-            reverse(matrix[i].begin(),matrix[i].end());
+           int s=0; int e=matrix[i].size()-1;
+           while(s<e){
+            swap(matrix[i][s],matrix[i][e]);
+            s++; e--;
+           }
         }
         
     }
