9d3dda7a5d0b7972f403e042fa2b0f7bb516d2a7
RITIK TAYAL <tayalritik1002@gmail.com>
2024-11-21 13:29:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (13.21%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-long long fact(int n,int r){
-     long long res = 1;
+vector<int>generate_rows(int n){
+    long long ans=1;
+    vector<int>row;
+    row.push_back(1);
+    for(int col=1;col<n;col++){
+        ans=ans*(n-col);
+        ans=ans/col;
+        row.push_back(ans);
 
-   for (int i = 0; i <r ; i++) {
-        res = res * (n-i);
-        res=res/(i+1);
     }
-
-return res;
+    return row;
 
 }
     vector<vector<int>> generate(int numRows) {
 
         vector<vector<int>>ans;
-  
-
-        for(int row=0;row<numRows;row++){
-            vector<int>curr;
-            for(int col=0;col<=row;col++){
-                long long curr_ans= fact(row,col);
-                curr.push_back(curr_ans);
-
-            }
-            ans.push_back(curr);
+        for(int i=1;i<=numRows;i++){
+            vector<int>ans2=generate_rows(i);
+            ans.push_back(ans2);
         }
-        return ans;  
+        return ans;
     }
 };
