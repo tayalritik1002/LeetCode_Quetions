832b62761f0af94b11b639513f702ce25347382e
RITIK TAYAL <tayalritik1002@gmail.com>
2024-11-21 14:19:12 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (60.92%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int>ans;int n=nums.size();
 
-        int mini=n/3+1;
-        unordered_map<int,int>mp;
+        int ele1,ele2;
+        int cnt1=0; int cnt2=0; vector<int>ans;
 
-
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-
-            if(mp[nums[i]]==mini){
-                ans.push_back(nums[i]);
+        for(int i=0;i<nums.size();i++){
+            if(cnt1==0 && nums[i]!=ele2){
+                ele1=nums[i]; cnt1=1;
+            }
+             else if(cnt2==0 && nums[i]!=ele1){
+                ele2=nums[i]; cnt2=1;
+            } 
+            else if(nums[i]==ele1)cnt1++;
+            else if(nums[i]==ele2)cnt2++;
+            else{
+                cnt1--;cnt2--;
             }
 
-            if(ans.size()>=2)break;
+        }
+        cnt1=0; cnt2=0;
 
-        }  
-        return ans;      
+        for(int i=0;i<nums.size();i++){
+            if(ele1==nums[i])cnt1++;
+            else if(ele2==nums[i])cnt2++;
+        }
+        int mini=nums.size()/3+1; cout<<cnt1<<" "<< cnt2<<endl;
+        if(cnt1>=mini)ans.push_back(ele1);
+         if(cnt2>=mini)ans.push_back(ele2);
+
+        return ans;
+        
     }
 };
