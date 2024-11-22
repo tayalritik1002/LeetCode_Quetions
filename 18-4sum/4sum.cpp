2db10ca4bcc7c54593b9ec2a4b1cd155f0e79926
RITIK TAYAL <tayalritik1002@gmail.com>
2024-11-22 13:01:55 +0530

    Time: 1705 ms (5.00%) | Memory: 508 MB (5.03%) - LeetSync

---
+++ 18-4sum/4sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        set<vector<int>>st; int n=nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                set<long long>st2;
+                for(int k=j+1;k<n;k++){
+                    long long sum=nums[i]+nums[j];
+                    sum+=nums[k];
+                    long long fourth=target-sum;
+
+                    if(st2.find(fourth)!=st2.end()){
+                        vector<int>temp={nums[i],nums[j],nums[k],(int)(fourth)};
+                        sort(temp.begin(),temp.end());
+                        st.insert(temp); 
+                    }
+                    st2.insert(nums[k]);
+                }
+
+            }
+        }
+        vector<vector<int>>ans(st.begin(),st.end());
+        return ans;
+        
+    }
+};
