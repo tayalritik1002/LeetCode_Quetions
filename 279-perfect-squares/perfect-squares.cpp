d43010fdbc160063cd885ad1973970b9bb8720a4
RITIK TAYAL <tayalritik1002@gmail.com>
2024-12-03 17:35:35 +0530

    Time: 43 ms (74.29%) | Memory: 13.2 MB (24.40%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-int solve(int n , vector<int>&dp){
-    if(n==0)return 0;
-    if(dp[n]!=-1)return dp[n];
-    int ans=n;
-    for(int i=1;i*i<=n;i++){
-        ans=min(ans,1+solve(n-i*i,dp));
-
-    }
-    dp[n]=ans;
-    return dp[n];
-
-}
     int numSquares(int n) {
-        vector<int>dp(n+1,-1);
-
-        return solve(n,dp);
+         vector<int>dp(n+1,INT_MAX);
+        dp[0]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j*j<=i;j++){
+                int temp=j*j;
+                // if(i-temp>=0)
+                dp[i]=min(dp[i],1+dp[i-temp]);
+            }
+            
+        }
+        return dp[n];
         
     }
+        
+    
 };
