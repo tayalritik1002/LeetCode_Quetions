25b5af8af019f2fbfeaa69b19fba4d0f95739fd7
RITIK TAYAL <tayalritik1002@gmail.com>
2024-12-05 11:21:57 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (8.08%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     int countNumbersWithUniqueDigits(int n) {
+        if(n==0)return 1;
+        int count_unique=10; // unique digit for n==1;
+        if(n==1)return count_unique;
+        int temp=9,curr=9;
+        for(int i=2;i<=n;i++){
+            curr*=temp;
+            count_unique+=curr;
+            temp--;
 
-        int prev_D=0;
-        int prev_ND=1; long long d,nd;
-        int prev_noOfDigits=0;
-
-        vector<int>ans(n+1,1);
-        for(int i=1;i<=n;i++){
-            d=prev_D*10+prev_noOfDigits*prev_ND;
-            nd=pow(10,i)-pow(10,i-1)-d;
-            cout<<nd;
-            ans[i]=ans[i-1]+nd;
-            cout<<ans[i];
-            prev_D=d;
-            prev_ND=nd;
-            prev_noOfDigits++;
         }
-        return ans[n];
-
-        
+        return count_unique;    
     }
 };
