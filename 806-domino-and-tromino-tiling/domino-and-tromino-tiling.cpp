cb2b4b5171d8d2d5bf1c4986087ae4b18ffb36b2
RITIK TAYAL <tayalritik1002@gmail.com>
2024-12-08 13:56:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (66.38%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int numTilings(int n) {
-        long long M = 1e9 + 7, a = 1, b = 1, c = n == 1 ? 1 : 2;
-        for (int i = 2, s = 0; i <= n; i++) {
-            c = (a + b + 2 * s) %
-                M; //  if
-                   //  dp[n]=dp[n-1]+dp[n-2]+2*dp[n-3]+2*dp[n-4]........2*dp[0];
-                   // s is the sum of 2*dp[n-3]+2*dp[n-4].......2*dp[0];
-            cout << c;
-            s = (s + a) % M;
-            a = b; b = c;
+        long long M = 1e9 + 7, a = 1, b = 2, c = 5;
+        if(n==1)return a;
+        if(n==2)return b;
+        if(n==3)return c;
+        int d;
+        for(int i=4;i<=n;i++){
+            d=((2*c)+a)%M;
+            a=b;
+            b=c;
+            c=d;
         }
-        return c;
+        return d;   
     }
 };
