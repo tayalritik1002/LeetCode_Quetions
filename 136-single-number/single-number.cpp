62fa44621440f7a2a7481e8d28de672f0b3c0d4c
RITIK TAYAL <tayalritik1002@gmail.com>
2024-12-29 19:34:57 +0530

    Time: 11 ms (16.85%) | Memory: 24 MB (10.81%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int xo=0;
-        for(int i=0;i<nums.size();i++){
-            xo^=nums[i];
+        // int xo=0;
+        // for(int i=0;i<nums.size();i++){
+        //     xo^=nums[i];
+        // }
+        // return xo;
+
+        unordered_map<int,int>mp;
+        for(auto s:nums){
+            mp[s]++;
         }
-        return xo;
-        
+        int ans;
+
+        for(auto i:mp){
+            if(i.second==1){ans= i.first;
+            break;}
+        }
+        return ans;
+
     }
 };
