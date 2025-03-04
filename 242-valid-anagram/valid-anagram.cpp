5091b31fe4827cabb89d0052c1c2d92cd0f1d9ca
RITIK TAYAL <tayalritik1002@gmail.com>
2025-03-04 05:48:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (48.15%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int count[26]={0};
-        for(char x:s){
-            count[x-'a']++;
-        }
-        for(char x:t){
-            count[x-'a']--;
-        }
-        for(int p:count){
-            if(p!=0)return false;
-        }
-        return true;
-        
+      int count[26]={0};
+      for(auto x:s)count[x-'a']++;
+      for(auto x:t)count[x-'a']--;
+      for(auto x:count){
+        if(x!=0)return false;
+      }
+      return true;
     }
 };
