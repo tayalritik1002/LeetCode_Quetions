b1feb3ff29a19c8d789ed9ef6c58eae6fa9006ae
RITIK TAYAL <tayalritik1002@gmail.com>
2025-03-16 12:15:02 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (71.04%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        bool flag=false; int i;
+        int xor1=0; int xor2=0;
+        int n=nums.size();
 
-
-        for( i=0;i<=nums.size();i++){
-            for(int j=0;j<nums.size();j++){
-               if(i==nums[j]){
-                flag=true;
-                break;
-               }
-               else flag=false;
-            }
-            if(!flag)break;
-            cout<<flag;
+        for(int i=0;i<n;i++){
+            xor1=xor1^nums[i];
+            xor2=xor2^(i+1);
         }
-        return i;
-    
-    }
+        // xor2^=n;
+        return xor1^xor2;
         
-    
+    }
 };
