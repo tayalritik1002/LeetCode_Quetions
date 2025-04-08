feb4fbcb95f424cf37c0d3100275dcc99e367201
RITIK TAYAL <tayalritik1002@gmail.com>
2025-04-08 05:33:09 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (39.83%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     string frequencySort(string s) {
-
-        unordered_map<char, int>mp;
-        for(auto sp:s)mp[sp]++;
-        string ans="";
-
-        vector<pair<char,int>>v(mp.begin(),mp.end());
-
-        sort(v.begin(),v.end(), [](auto a,auto b){return a.second>b.second;});
-        for(auto it:v){
-            int count=it.second;
-            char ch=it.first;
-            ans+=string(count,ch);
+        unordered_map<char,int>charMap;
+        int n1 = s.size();
+        for(char c : s){
+            charMap[c]++;
         }
-
-        return ans;
-        
+        string result = "";
+        priority_queue<pair<int,char>> pq;
+        for(auto& it:charMap){
+            pq.push({it.second, it.first});
+        }
+        while(!pq.empty()){
+            auto[freq,ch] = pq.top();
+            pq.pop();
+            result.append(freq,ch);
+        }
+        return result;
     }
 };
