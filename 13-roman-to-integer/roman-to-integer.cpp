26ca027f72cfe73d50b2692fcc9089fb132f4914
RITIK TAYAL <tayalritik1002@gmail.com>
2025-04-09 06:32:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (98.92%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,43 +1,49 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int ans = 0;
-        int n = s.size();
-        for (int i = 0; i < n; i++) {
-            if (s[i] == 'I' && s[i + 1] == 'V') {
-                ans = ans + 4;
-                i++;
-            } else if (s[i] == 'I' && s[i + 1] == 'X') {
-                ans = ans + 9;
-                i++;
-            } else if (s[i] == 'X' && s[i + 1] == 'L') {
-                ans = ans + 40;
-                i++;
-            } else if (s[i] == 'X' && s[i + 1] == 'C') {
-                ans = ans + 90;
-                i++;
-            } else if (s[i] == 'C' && s[i + 1] == 'D') {
-                ans = ans + 400;
-                i++;
-            } else if (s[i] == 'C' && s[i + 1] == 'M') {
-                ans = ans + 900;
-                i++;
-            } else if (s[i] == 'I') {
-                ans = ans + 1;
-            } else if (s[i] == 'V') {
-                ans = ans + 5;
-            } else if (s[i] == 'X') {
-                ans = ans + 10;
-            } else if (s[i] == 'L') {
-                ans = ans + 50;
-            } else if (s[i] == 'C') {
-                ans = ans + 100;
-            } else if (s[i] == 'D') {
-                ans = ans + 500;
-            } else if (s[i] == 'M') {
-                ans = ans + 1000;
+        int ans=0;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            switch(s[i]){
+                case 'I':
+                ans+=1;
+                break;
+
+                case 'V':
+                ans+=5;
+                if(i>0 && s[i-1]=='I')ans-=2;
+                break;
+                
+                case 'X':
+                ans+=10;
+                if(i>0 && s[i-1]=='I')ans-=2;
+                break;
+                
+                case 'L':
+                ans+=50;
+                if(i>0 && s[i-1]=='X')ans-=20;
+                break;
+                
+                case 'C':
+                ans+=100;
+                if(i>0 && s[i-1]=='X')ans-=20;
+                break;
+                
+                case 'D':
+                ans+=500;
+                if(i>0 && s[i-1]=='C')ans-=200;
+                break;
+                
+                case 'M':
+                ans+=1000;
+                if(i>0 && s[i-1]=='C')ans-=200;
+                break;
+
+                default: break;
+                
             }
         }
         return ans;
+        
     }
 };
