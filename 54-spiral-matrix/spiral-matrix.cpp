f9ce3ea8c598391c2228d97c23df7c6b50d33703
RITIK TAYAL <tayalritik1002@gmail.com>
2025-04-20 15:24:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (50.12%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-          int row=matrix.size();
+        int row=matrix.size();
         int col=matrix[0].size();
         int srow=0;
         int erow=row-1;
-        int scol=0;
-        int ecol=col-1;
+        int scol=0, ecol=col-1;
         int total=row*col;
         int count=0;
         vector<int>ans;
-        while(count<total){
-            for(int index=srow;index<=ecol && count<total;index++ ){
-                ans.push_back(matrix[srow][index]); count++;
+        while(total>count){
+            for(int i=srow;total>count && i<=ecol;i++){
+                ans.push_back(matrix[srow][i]); count++;
             }
             srow++;
-             for(int index=srow;index<=erow && count<total;index++ ){
-                ans.push_back(matrix[index][ecol]); count++;
+            for(int i=srow;i<=erow && total>count;i++){
+                ans.push_back(matrix[i][ecol]);count++;
             }
             ecol--;
-             for(int index=ecol;index>=scol && count<total;index-- ){
-                ans.push_back(matrix[erow][index]); count++;
+            for(int i=ecol;i>=scol && total>count;i--){
+                ans.push_back(matrix[erow][i]); count++;
+
             }
             erow--;
-             for(int index=erow;index>=srow && count<total;index-- ){
-                ans.push_back(matrix[index][scol]); count++;
+            for(int i=erow;i>=srow && total>count;i--){
+                ans.push_back(matrix[i][scol]);count++;
             }
             scol++;
-        }
 
+
+        }
         return ans;
+        
     }
 };
