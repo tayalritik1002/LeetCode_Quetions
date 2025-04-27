e9b94a308b5c4dc621df4461fc5e7258861a7712
RITIK TAYAL <tayalritik1002@gmail.com>
2025-04-27 07:30:44 +0530

    Time: 153 ms (5.02%) | Memory: 118.7 MB (5.03%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+  void combo(vector<int>arr,vector<vector<int>>&ans,vector<int>store,int index,int target){
+if(target==0){
+    ans.push_back(store);
+    return;
+}
+  if(target<0 || index>=arr.size()){
+    return ;
+  }
+  store.push_back(arr[index]);
+  combo(arr,ans,store,index,target-arr[index]);
+  store.pop_back();
+  combo(arr,ans,store,index+1,target);  
+  }   
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    int index=0;
+    vector<vector<int>>ans;
+    vector<int>store;
+    int sum=0;
+    combo(candidates,ans,store,index,target);
+    
+    return ans;
+    
+    }
+};
