82aa6fa6620c16d737d734200b17f557de7cd4f9
RITIK TAYAL <tayalritik1002@gmail.com>
2026-05-23 15:05:44 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (82.96%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int xor1=nums[0];
-        for(int i=1;i<nums.size();i++){
-            xor1=xor1^nums[i];
+        int xor1 = nums[0];
+        for(int i = 1; i<nums.size(); i++){
+            xor1 = xor1 ^ nums[i];
         }
         return xor1;
         
